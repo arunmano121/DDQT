1142eb4cb2f73e35dcd6a8fda8bd0de8b6ee1b00
AM <am@ampro.com>
2021-03-12 15:15:14 -0800

    Added comments and docstrings to point_in_convex_polygon

--- point_in_convex_polygon.py
+++ point_in_convex_polygon.py
@@ -1,17 +1,53 @@
-# Helper module to determine if a point lies within a polygon
+'''Helper module to determine if a point lies within a polygon
 
-# original source:
-# https://stackoverflow.com/questions/2752725/finding-whether-a-point-lies-inside-a-rectangle-or-not
-# https://algorithmtutor.com/Computational-Geometry/Check-if-a-point-is-inside-a-polygon/
+original source:
+https://stackoverflow.com/questions/2752725/finding-whether-a-point-lies-inside-a-rectangle-or-not
+
+https://algorithmtutor.com/Computational-Geometry/Check-if-a-point-is-inside-a-polygon/
+
+Modified for use.
+'''
 
 
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    '''Helper Point class
+
+    Point class to define a point and a helper function to determine if
+    the point lies with a polygon.
+    '''
+
+    def __init__(self, s1, s2):
+        '''Initialization function for the class
+
+        Parameters:
+
+        - s1: s1 coordinate of point
+        - s2: s2 coordinate of point
+
+        Returns:
+
+        - None
+        '''
+
+        self.s1 = s1
+        self.s2 = s2
+
+        return
 
 
 def is_within_polygon(polygon, point):
+    '''Determine if a point lies withinthe polygon
+
+    Parameters:
+
+    - polygon: polygon definition using a set of points
+    - point: a single point
+
+    Returns:
+
+    - True/False: Depending on if point lies within polygon
+    '''
+
     A = []
     B = []
     C = []
@@ -21,9 +57,9 @@ def is_within_polygon(polygon, point):
         p2 = polygon[(i + 1) % len(polygon)]
 
         # calculate A, B and C
-        a = -(p2.y - p1.y)
-        b = p2.x - p1.x
-        c = -(a * p1.x + b * p1.y)
+        a = -(p2.s2 - p1.s2)
+        b = p2.s1 - p1.s1
+        c = -(a * p1.s1 + b * p1.s2)
 
         A.append(a)
         B.append(b)
@@ -31,18 +67,24 @@ def is_within_polygon(polygon, point):
 
     D = []
     for i in range(len(A)):
-        d = A[i] * point.x + B[i] * point.y + C[i]
+        d = A[i] * point.s1 + B[i] * point.s2 + C[i]
         D.append(d)
 
     t1 = all(d >= 0 for d in D)
     t2 = all(d <= 0 for d in D)
+
     return t1 or t2
 
 
 if __name__ == '__main__':
+    # Example usage:
+    # define a polygon containing as many points representing vertices
     polygon = [Point(0, 0), Point(5, 0), Point(6, 7),
                Point(2, 3), Point(0, 4)]
+
+    # define a point p1
     p1 = Point(1, 1)
+    # determine if p1 lies within the polygon
     print(is_within_polygon(polygon, p1))  # returns True
     p2 = Point(6, 2)
     print(is_within_polygon(polygon, p2))  # returns False
